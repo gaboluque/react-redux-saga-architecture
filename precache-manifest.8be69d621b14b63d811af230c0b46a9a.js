2683125b1dbe3af5ecbead0b5dd2fe4e261ca2c6
Gabriel Luque <gluque>
2021-02-09 12:24:53 -0500

    Updates

---
+++ precache-manifest.8be69d621b14b63d811af230c0b46a9a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d978f2c8e5e8a8d50fb4151b66e74543",
+    "url": "/react-redux-saga-architecture/index.html"
+  },
+  {
+    "revision": "d0f4ec325fe397fd993c",
+    "url": "/react-redux-saga-architecture/static/css/2.3c469aef.chunk.css"
+  },
+  {
+    "revision": "66ab61f294fae72ba984",
+    "url": "/react-redux-saga-architecture/static/css/3.2a225ffe.chunk.css"
+  },
+  {
+    "revision": "b0c3cecbc1946eb914eb",
+    "url": "/react-redux-saga-architecture/static/css/4.7b557102.chunk.css"
+  },
+  {
+    "revision": "174f0fdafc7ae29171c5",
+    "url": "/react-redux-saga-architecture/static/css/7.84dbfc17.chunk.css"
+  },
+  {
+    "revision": "c1bdc6faf5ebeed60e57",
+    "url": "/react-redux-saga-architecture/static/css/8.6fa2e78a.chunk.css"
+  },
+  {
+    "revision": "693a7bac3a3cd31b72c9",
+    "url": "/react-redux-saga-architecture/static/css/main.614caddd.chunk.css"
+  },
+  {
+    "revision": "d0f4ec325fe397fd993c",
+    "url": "/react-redux-saga-architecture/static/js/2.e069fefa.chunk.js"
+  },
+  {
+    "revision": "66ab61f294fae72ba984",
+    "url": "/react-redux-saga-architecture/static/js/3.fe886ef7.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "/react-redux-saga-architecture/static/js/3.fe886ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c3cecbc1946eb914eb",
+    "url": "/react-redux-saga-architecture/static/js/4.4a32a2ff.chunk.js"
+  },
+  {
+    "revision": "3af8a6635e85ccc6e924",
+    "url": "/react-redux-saga-architecture/static/js/5.71c96a24.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-redux-saga-architecture/static/js/5.71c96a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f61b99c0e53058d8c7",
+    "url": "/react-redux-saga-architecture/static/js/6.bb1f76b3.chunk.js"
+  },
+  {
+    "revision": "174f0fdafc7ae29171c5",
+    "url": "/react-redux-saga-architecture/static/js/7.afd25500.chunk.js"
+  },
+  {
+    "revision": "c1bdc6faf5ebeed60e57",
+    "url": "/react-redux-saga-architecture/static/js/8.23c2e44d.chunk.js"
+  },
+  {
+    "revision": "7995334347d91dcfcf9a",
+    "url": "/react-redux-saga-architecture/static/js/9.a6a37cf7.chunk.js"
+  },
+  {
+    "revision": "693a7bac3a3cd31b72c9",
+    "url": "/react-redux-saga-architecture/static/js/main.56013054.chunk.js"
+  },
+  {
+    "revision": "fc022d610c6446412ae0",
+    "url": "/react-redux-saga-architecture/static/js/runtime-main.3ce83497.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-redux-saga-architecture/precache-manifest.8be69d621b14b63d811af230c0b46a9a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-saga-architecture/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
