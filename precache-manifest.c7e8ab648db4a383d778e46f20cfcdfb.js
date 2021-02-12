0b5630ace07eef3cd9d41b01b4cd3a8fabffbd7e
Gabriel Luque <gluque>
2021-02-12 14:03:51 -0500

    Updates

---
+++ precache-manifest.c7e8ab648db4a383d778e46f20cfcdfb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c9b630e378ac9e2a3f72f7a400d910",
+    "url": "/react-redux-saga-architecture/index.html"
+  },
+  {
+    "revision": "58c144fa7092d2926a9f",
+    "url": "/react-redux-saga-architecture/static/css/2.e9eac0d3.chunk.css"
+  },
+  {
+    "revision": "6e63f19596c1cf667bb6",
+    "url": "/react-redux-saga-architecture/static/css/3.4481e823.chunk.css"
+  },
+  {
+    "revision": "cee119e166ce1995d70a",
+    "url": "/react-redux-saga-architecture/static/css/6.040c2641.chunk.css"
+  },
+  {
+    "revision": "a9d05fd748fb6287e724",
+    "url": "/react-redux-saga-architecture/static/css/7.6fa2e78a.chunk.css"
+  },
+  {
+    "revision": "075af3330a0c92975719",
+    "url": "/react-redux-saga-architecture/static/css/main.8fcd330e.chunk.css"
+  },
+  {
+    "revision": "58c144fa7092d2926a9f",
+    "url": "/react-redux-saga-architecture/static/js/2.4a853659.chunk.js"
+  },
+  {
+    "revision": "6e63f19596c1cf667bb6",
+    "url": "/react-redux-saga-architecture/static/js/3.7481f849.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "/react-redux-saga-architecture/static/js/3.7481f849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f622d2c144a35cccb9",
+    "url": "/react-redux-saga-architecture/static/js/4.c339c057.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-redux-saga-architecture/static/js/4.c339c057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6e8b0600c388d8f6454",
+    "url": "/react-redux-saga-architecture/static/js/5.580de40e.chunk.js"
+  },
+  {
+    "revision": "cee119e166ce1995d70a",
+    "url": "/react-redux-saga-architecture/static/js/6.91b60cc1.chunk.js"
+  },
+  {
+    "revision": "a9d05fd748fb6287e724",
+    "url": "/react-redux-saga-architecture/static/js/7.d8071a0a.chunk.js"
+  },
+  {
+    "revision": "acd9cdc6856e689ac207",
+    "url": "/react-redux-saga-architecture/static/js/8.d7ab1454.chunk.js"
+  },
+  {
+    "revision": "075af3330a0c92975719",
+    "url": "/react-redux-saga-architecture/static/js/main.c8262d53.chunk.js"
+  },
+  {
+    "revision": "0cbc91d4c5e78757cf33",
+    "url": "/react-redux-saga-architecture/static/js/runtime-main.dac1bb91.js"
+  }
+]);
